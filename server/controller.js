f04c61c487d7cefe7e2ec350f6a7a3c580e3ad99
Shawn Turley <antonioturley@gmail.com>
2019-05-27 16:56:04 -0600

    started login

--- server/controller.js
+++ server/controller.js
@@ -4,16 +4,21 @@ module.exports = {
 	register(req, res) {
 		const db = req.app.get('db');
 		const { username, password, profile_pic } = req.body;
-		console.log(username, password)
 
 		db.createUser({ username, password, profile_pic })
 		.then(userId => {
 			const id = userId[0].id
-			req.session.id
+			req.session.id = id
 			res.status(200).send({ username, id, profile_pic })
 		}).catch(err => console.log(err))
 
 	},
 
+	login(req, res) {
+		const db = req.app.get('db');
+		const { username, password } = req.body;
+
+		db.checkForUser({username})
+	}
 
 }
--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -4,13 +4,7 @@ import './dashboard.css';
 function Dashboard(props) {
 	return (
 		<div>
-			<div className="box-model">Box Model</div>
-
-			<div className="float-display-parent">
-				<div className="float-display"></div>
-				<div className="float-display"></div>
-				<div className="float-display"></div>
-			</div>
+			dashboard
 		</div>
 	)
 }
